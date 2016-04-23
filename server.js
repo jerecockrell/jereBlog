fb75b64e05eb70416738f7928f34bbacbe6df729
jerecockrell <jere.cockrell@gmail.com>
2016-04-23 15:17:29 -0600

    set up webpack configuration and add simple react component to test if actually working

--- server.js
+++ server.js
@@ -14,6 +14,24 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+if (process.env.NODE_ENV === 'production') {
+  console.log('Running in production mode');
+
+  app.use('/static', express.static('static'));
+} else {
+  // When not in production, enable hot reloading
+
+  var chokidar = require('chokidar');
+  var webpack = require('webpack');
+  var webpackConfig = require('./webpack.config.dev');
+  var compiler = webpack(webpackConfig);
+  app.use(require('webpack-dev-middleware')(compiler, {
+    noInfo: true,
+    publicPath: webpackConfig.output.publicPath
+  }));
+  app.use(require('webpack-hot-middleware')(compiler));
+}
+
 app.get('/', function(req, res){
 	res.sendFile(path.join(__dirname, 'index.html'));
 });
