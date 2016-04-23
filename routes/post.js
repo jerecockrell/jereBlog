6f5ddf31b29c21a9b9cd741cca60907442e31245
jerecockrell <jere.cockrell@gmail.com>
2016-04-23 14:58:58 -0600

    delete single post

--- routes/post.js
+++ routes/post.js
@@ -60,6 +60,15 @@ router.route('/:post_id')
 			})
 		})
 	})
+	.delete(function(req, res){
+		Post.remove( { _id: req.params.post_id }, function(err){
+			if(err) {
+				console.log(err)
+			} else {
+				res.json({ message: "POST deleted"})
+			}
+		}) 
+	});
 
 
 module.exports = router;
